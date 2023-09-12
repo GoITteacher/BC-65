d09134a535d04174e153f3dee062e006f0407dfa
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-09-12 21:44:56 +0300

    update

---
+++ src/js/01-script.js
@@ -0,0 +1,74 @@
+import { NewsAPI } from './modules/newsApi';
+
+const newsApi = new NewsAPI();
+let maxPage = 1;
+
+const refs = {
+  formElem: document.querySelector('.js-search-form'),
+  articleListElem: document.querySelector('.js-article-list'),
+  btnLoadMore: document.querySelector('.js-btn-load'),
+};
+
+refs.formElem.addEventListener('submit', onFormSubmit);
+refs.btnLoadMore.addEventListener('click', onLoadMoreClick);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const query = e.target.elements.query.value;
+  newsApi.query = query;
+  newsApi.page = 1;
+  newsApi.getArticles().then(data => {
+    maxPage = Math.ceil(data.totalResults / newsApi.pageSize);
+    refs.articleListElem.innerHTML = '';
+    renderArticles(data.articles);
+    refs.btnLoadMore.disabled = false;
+    updateStatusBtn();
+  });
+}
+
+function onLoadMoreClick(e) {
+  newsApi.page += 1;
+  newsApi.getArticles().then(data => {
+    renderArticles(data.articles);
+    updateStatusBtn();
+  });
+}
+
+function updateStatusBtn() {
+  if (newsApi.page === maxPage) {
+    refs.btnLoadMore.disabled = true;
+  }
+}
+
+function templateArticle({
+  author,
+  title,
+  description,
+  urlToImage,
+  publishedAt,
+}) {
+  return `
+  <li class="card news-card">
+        <img loading="lazy"
+          class="news-image"
+          src="${urlToImage}"
+          alt="${title}"
+        />
+        <h3 class="card-title">
+          ${title}
+        </h3>
+        <p class="card-desc">
+        ${description}
+        </p>
+        <div class="card-footer">
+          <span>${author}</span>
+          <span>${publishedAt}</span>
+        </div>
+      </li>`;
+}
+
+function renderArticles(articles) {
+  const markup = articles.map(templateArticle).join('');
+  // refs.articleListElem.innerHTML = markup;
+  refs.articleListElem.insertAdjacentHTML('beforeend', markup);
+}
--- src/js/modules/newsAPI.js
+++ src/js/modules/newsAPI.js
@@ -1,24 +1,25 @@
-const BASE_URL = 'https://free-news.p.rapidapi.com/v1';
-const END_POINT = '/search';
+const BASE_URL = 'https://newsapi.org';
+const END_POINT = '/v2/everything';
+const KEY = 'c8747511a2c34730a83caaff4f3693e7';
 
-const options = {
-  headers: {
-    'X-RapidAPI-Key': '9b3ff61931msh1b42d77d34e33dap1c29cajsn3d3169e0e2f4',
-    'X-RapidAPI-Host': 'free-news.p.rapidapi.com',
-  },
-};
-
-export class NewsApi {
+export class NewsAPI {
   query = '';
+  #pageSize = 10;
   page = 1;
-  constructor() {}
 
-  getNews(query) {
-    if (query) this.query = query;
+  getArticles() {
+    const PARAMS = new URLSearchParams({
+      apiKey: KEY,
+      q: this.query,
+      pageSize: this.#pageSize,
+      page: this.page,
+    });
 
-    return fetch(
-      `${BASE_URL}${END_POINT}?q=${this.query}&page=${this.page}`,
-      options,
-    ).then(response => response.json());
+    const url = `${BASE_URL}${END_POINT}?${PARAMS}`;
+    return fetch(url).then(res => res.json());
+  }
+
+  get pageSize() {
+    return this.#pageSize;
   }
 }
