1aba5dc724c7cd03795af8426055e0d5a36c70be
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-09-12 21:05:28 +0300

    update

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+import './modules/binance';
+import './modules/hero';
+import './modules/quotes';
+import './modules/ipfinder';
+import './modules/pokemon';
+import './modules/instagram';
+import './modules/user';
