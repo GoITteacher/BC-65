505cb26cb2c15c24e0a54ed89434d26f667cb2ca
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-11-06 13:44:56 +0200

    update

--- src/scripts/script1.js
+++ src/scripts/script1.js
@@ -4,13 +4,3 @@ const refs = {
   inputSizeElem: document.querySelector('#js-input-size'),
   inputColorElem: document.querySelector('#js-input-color'),
 };
-
-refs.inputSizeElem.addEventListener('input', e => {
-  const size = e.target.value;
-  refs.inputColorElem.style.transform = `scale(${size})`;
-});
-
-refs.inputColorElem.addEventListener('change', e => {
-  const color = e.target.value;
-  refs.formElem.style.backgroundColor = color;
-});
